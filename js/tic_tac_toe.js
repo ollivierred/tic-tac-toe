6460f88ed98d818b9eee554babba1277cd87e7e7
ollie <ollivierre.dujoh@gmail.com>
2017-11-04 13:51:49 -0500

    commit @1:51PM

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -31,52 +31,91 @@
 
     //Controls tic-tac-toe board events
     (function board(){
-        var player1 = document.querySelector("#player1");
-        var player2 = document.querySelector("#player2");
+        var p1 = document.querySelector("#player1");
+        var p2 = document.querySelector("#player2");
         var boxes = document.querySelector(".boxes");
         var $numOfBoxes = $(".box").length;
         var movesLeft = $numOfBoxes;
+        playerX = [];
+        playerO = [];
+        // Track each box filled, record its location on the board, and what it was filled with
 
-        var player = 1;                                     // Decides who goes first: p1 || p2
+        var turn = 1;                                     // Decides who goes first: p1 || p2
         player1.className = "players active";
 
-        function addPlayerToken(theBox) {
+        function addPlayerToken(theBox , tokenPosition) {
             var $currentBox = $(theBox);
 
             if ($currentBox.hasClass("box-filled-1") || $currentBox.hasClass("box-filled-2")) {
                 console.log("This box is filled");
                 return;
             } else {
-                if (player === 1) {                         // Player "O"
+                if (turn === 1) {                         // Player "O"
                     $currentBox.addClass("box-filled-1");
-                    player1.className = "players";
-                    player2.className = "players active";
-                    player += 1;
-                } else if (player === 2) {                  // Player "X"
+                    var filled = {o: tokenPosition};      // Coordinates of the current box stored in array "filled"
+                    playerO.push(filled);                 // Pushes filled array to the player's array
+                    p1.className = "players";
+                    p2.className = "players active";
+                    turn += 1;
+                    for (var i=0; i < playerO.length; i+=1) {
+                        console.log(playerO[i]);
+                    }
+                } else if (turn === 2) {                  // Player "X"
                     $currentBox.addClass("box-filled-2");
-                    player1.className = "players active";
-                    player2.className = "players";
-                    player -= 1;
+                    var filled = {x: tokenPosition};      // Coordinates of the current box stored in array "filled"
+                    playerX.push(filled);                 // Pushes filled array to the player's array
+                    p1.className = "players active";
+                    p2.className = "players";
+                    turn -= 1;
+                    for (var i=0; i < playerX.length; i+=1) {
+                        console.log(playerX[i]);
+                    }
                 }
             }
-            console.log("Your turn player " + player);
+            console.log("Your turn player " + turn);
+
+
         }
 
         
         boxes.addEventListener("click", function(e) {
             var thisBox = e.target;
-            addPlayerToken(thisBox);
+            var tokenPosition = thisBox.getAttribute("data-pos");
+
+            addPlayerToken(thisBox, parseInt(tokenPosition));
+
+
+            // Check for winner using patterns
+            // var winPatterns:
+            
+            //  horizontal
+
+            //  x  y  x  y  x  y
+            //  0, 0  0, 1  0, 2 
+            //  1, 0  1, 1  1, 2
+            //  2, 0  2, 1  2, 2
+
+            //  vertical
+            //  x  y  x  y  x  y
+            //  0, 0  1, 0  2, 0
+            //  0, 1  1, 1  2, 1
+            //  0, 2  1, 2  2, 2 
+
+
+            //  diagonal = x:[1], y:[3]
+            //  diagonal = x:[2], y:[2]
+            //  diagonal = x:[3], y:[1]
+
+            // Check for winner || Tie
+            // if player "X" has 3 in a row: horizontally || vertically || diagonally
+            // if player "O" has 3 in a row: horizontally || vertically || diagonally
+            // else it is a tie
+            // Prepare screenWin and message
 
             (function gameFinish() {
                 movesLeft -= 1;
                 console.log("number of moves left " + movesLeft);
-                // Track each box filled, record its location on the board, and what it was filled with
 
-                // Check for winner || Tie
-                // if player "X" has 3 in a row: horizontally || vertically || diagonally
-                // if player "O" has 3 in a row: horizontally || vertically || diagonally
-                // else it is a tie
-                // Prepare screenWin and message
 
                 if(movesLeft === 0) {
                     // Controls events at the end of each game
