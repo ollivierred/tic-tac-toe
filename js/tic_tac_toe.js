1c0bb754e6ebfac63afa846b05b61c6704eb24fd
ollivierred <ollivierre.dev@gmail.com>
2017-11-06 09:43:05 -0600

    commit @9:42AM Mon, Nov 6

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,147 +1,138 @@
 
-(function() {
+
     //Screen states
     const gameBoard = document.querySelector("#board");
     const screenStart = document.querySelector("#start");
     const screenWin = document.querySelector("#finish");
 
     //Visibility of each screen
-    gameBoard.style.display = "none";
+    // gameBoard.style.display = "none";
     screenStart.style.display = "none";
     screenWin.style.display = "none";
+
+    // --------------------------------------------------------------
+
 
     //Buttons
     const newGameButton = document.querySelector("#finish a");
     const startGameButton = document.querySelector("#start a");
 
 
-    //Controls event on window load
-    window.addEventListener("load", function() {
-        screenStart.style.display = "inherit";
+    const p1 = document.querySelector("#player1");
+    const p2 = document.querySelector("#player2");
+    var player = 1;                                 // Declares who goes first
+        p1.className = "players active";
+    const playerOne = "O";
+    const playerTwo = "X";
+    const winPattern = [     
+        [0, 1, 2], [0, 3, 6], 
+        [3, 4, 5], [1, 4, 7],
+        [6, 7, 8], [2, 5, 8],
+        [0, 4, 8], [6, 4, 2]
+    ];
+    var plays = [];
 
-        (function gameStart(){
-            startGameButton.addEventListener("click", function() {
-                screenStart.style.display = "none";
-                gameBoard.style.display = "inherit";
-            });
-        }());
+    var activeBoard = [ 
+        null,null,null,
+        null,null,null,
+        null,null,null 
+    ];
 
-    }, false);
+    // var activeBoard = { 
+    //     0:null, 1:null, 2:null,
+    //     3:null, 4:null, 5:null,
+    //     6:null, 7:null, 8:null 
+    // };
+
+    function setupWinScreen(winner, screenMessage) {
+        var winner = winner;
+        var screenMessage = screenMessage;
+
+        //"screen-win-one", "screen-win-two", "screen-win-tie"
+        $(screenWin).addClass("screen-win-" + winner);
+        var message = document.querySelector(".message");
+        message.textContent = screenMessage;
+    }
+
+    function resetGame() {
+        var $box = $(".box");
+        $box.each( function () {
+            if ($box.hasClass("box-filled-1") || $box.hasClass("box-filled-2")) {
+                $box.removeClass("box-filled-1");
+                $box.removeClass("box-filled-2");
+            }
+        })
+    }
+
+    // Controls event on window load
+    // window.addEventListener("load", function() {
+    //     screenStart.style.display = "inherit";
+    // }, false);
+
+    // startGameButton.addEventListener("click", function() {
+    //      screenStart.style.display = "none";
+    //      gameBoard.style.display = "inherit";
+    // }, false);
 
 
     //Controls tic-tac-toe board events
-    (function board(){
-        var p1 = document.querySelector("#player1");
-        var p2 = document.querySelector("#player2");
-        var boxes = document.querySelector(".boxes");
-        var $numOfBoxes = $(".box").length;
-        var movesLeft = $numOfBoxes;
-        playerX = [];
-        playerO = [];
-        // Track each box filled, record its location on the board, and what it was filled with
 
-        var turn = 1;                                     // Decides who goes first: p1 || p2
-        player1.className = "players active";
+    
+    const board = document.querySelector(".boxes");
+    // board.addEventListener("mouseover", boxClick, false);
+    board.addEventListener("click", boxClick, false);
+    
 
-        function addPlayerToken(theBox , tokenPosition) {
-            var $currentBox = $(theBox);
+    // --------------------------------------------------------------
+    // BOXCLICK FUNCTION
+    // --------------------------------------------------------------
+    function boxClick(box){
+        const clickedBox = box.target;
+        playerTurn(clickedBox);
+    }
+    
+    // --------------------------------------------------------------
+    // TURN FUNCTION
+    // --------------------------------------------------------------
+    function playerTurn(clickedBox) {
+        const boxPos = clickedBox.getAttribute("data-pos");
+        const $clickedBox = $(clickedBox);
 
-            if ($currentBox.hasClass("box-filled-1") || $currentBox.hasClass("box-filled-2")) {
-                console.log("This box is filled");
-                return;
-            } else {
-                if (turn === 1) {                         // Player "O"
-                    $currentBox.addClass("box-filled-1");
-                    var filled = {o: tokenPosition};      // Coordinates of the current box stored in array "filled"
-                    playerO.push(filled);                 // Pushes filled array to the player's array
-                    p1.className = "players";
-                    p2.className = "players active";
-                    turn += 1;
-                    for (var i=0; i < playerO.length; i+=1) {
-                        console.log(playerO[i]);
-                    }
-                } else if (turn === 2) {                  // Player "X"
-                    $currentBox.addClass("box-filled-2");
-                    var filled = {x: tokenPosition};      // Coordinates of the current box stored in array "filled"
-                    playerX.push(filled);                 // Pushes filled array to the player's array
-                    p1.className = "players active";
-                    p2.className = "players";
-                    turn -= 1;
-                    for (var i=0; i < playerX.length; i+=1) {
-                        console.log(playerX[i]);
-                    }
-                }
+        if ($clickedBox.hasClass("box-filled-1") || $clickedBox.hasClass("box-filled-2")) {
+            return;
+        } else {
+            if (player === 1) {                         // Player "O"
+                activeBoard[boxPos] = playerOne;
+                $clickedBox.addClass("box-filled-1");
+                p1.className = "players";
+                p2.className = "players active";
+                player += 1;
+            } else if (player === 2) {                  // Player "X"
+                activeBoard[boxPos] = playerTwo;
+                $clickedBox.addClass("box-filled-2");
+                p1.className = "players active";
+                p2.className = "players";
+                player -= 1;
             }
-            console.log("Your turn player " + turn);
-
-
         }
+        console.log("Position " + boxPos);
+        console.log(activeBoard);
 
+        checkForWin(activeBoard, playerOne);
+    }
+
+    // --------------------------------------------------------------
+    // CHECKFORWIN FUNCTION
+    // --------------------------------------------------------------
+    function checkForWin(board, player) {
+        // debugger
+        let plays = board.reduce( function(acc, el, index) {
+            if(el === player) {
+                return acc.concat(index);
+            } else {
+                return acc;
+            }
+        }, []);
+        console.log(plays);
         
-        boxes.addEventListener("click", function(e) {
-            var thisBox = e.target;
-            var tokenPosition = thisBox.getAttribute("data-pos");
-
-            addPlayerToken(thisBox, parseInt(tokenPosition));
-
-
-            // Check for winner using patterns
-            // var winPatterns:
-            
-            //  horizontal
-
-            //  x  y  x  y  x  y
-            //  0, 0  0, 1  0, 2 
-            //  1, 0  1, 1  1, 2
-            //  2, 0  2, 1  2, 2
-
-            //  vertical
-            //  x  y  x  y  x  y
-            //  0, 0  1, 0  2, 0
-            //  0, 1  1, 1  2, 1
-            //  0, 2  1, 2  2, 2 
-
-
-            //  diagonal = x:[1], y:[3]
-            //  diagonal = x:[2], y:[2]
-            //  diagonal = x:[3], y:[1]
-
-            // Check for winner || Tie
-            // if player "X" has 3 in a row: horizontally || vertically || diagonally
-            // if player "O" has 3 in a row: horizontally || vertically || diagonally
-            // else it is a tie
-            // Prepare screenWin and message
-
-            (function gameFinish() {
-                movesLeft -= 1;
-                console.log("number of moves left " + movesLeft);
-
-
-                if(movesLeft === 0) {
-                    // Controls events at the end of each game
-                    gameBoard.style.display = "none";
-                    screenWin.style.display = "inherit";
-                    var message = document.querySelector(".message");
-                    message.textContent = "Its a tie";
-                }
-                newGameButton.addEventListener("click", function() {
-                    var $box = $(".box");
-                    $box.each( function () {
-                        if ($box.hasClass("box-filled-1") || $box.hasClass("box-filled-2")) {
-                            $box.removeClass("box-filled-1");
-                            $box.removeClass("box-filled-2");
-                        }
-                    })
-                    movesLeft = $numOfBoxes;
-                    screenWin.style.display = "none";
-                    gameBoard.style.display = "inherit";
-                });
-            }());
-
-        }, false);
-
-    }());
-
-}());
-
-
+    }
