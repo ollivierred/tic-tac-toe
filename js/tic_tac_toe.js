6c933395ea0a52b4e1219cb2f0b1b85c335082e0
ollivierred <ollivierre.dev@gmail.com>
2017-11-07 00:13:17 -0600

    commit @ 12:10AM Tue, Nov 7
    Missing components:
     - Condition when game is tied
     - Computer AI opponent
     - Mouse over function on game board

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -14,8 +14,8 @@
     const startGameButton = document.querySelector("#start a");
 
 
-    const p1 = document.querySelector("#player1");
-    const p2 = document.querySelector("#player2");
+    const $p1 = $("#player1");
+    const $p2 = $("#player2");
     const playerOne = "one";
     const playerTwo = "two";
     const winPattern = [     
@@ -26,7 +26,7 @@
     ];
 
     var player = 1;                     // Declares who goes first
-    p1.className = "players active";
+    $p1.addClass("active");
 
     var activeBoard = [ 
         null,null,null,
@@ -44,9 +44,8 @@
         })
 
         player = 1;
-        p1.className = "players active";
-        p2.className = "players";
-
+        $p1.addClass("active");
+        $p2.removeClass("active");
         activeBoard = [ null,null,null,null,null,null,null,null,null ];
 
     }
@@ -74,6 +73,7 @@
     function playerTurn(clickedBox) {
         const boxPos = clickedBox.getAttribute("data-pos");
         const $clickedBox = $(clickedBox);
+        var winner;
 
         if ($clickedBox.hasClass("box-filled-1") || $clickedBox.hasClass("box-filled-2")) {
             return;
@@ -81,17 +81,17 @@
             if (player === 1) {                         // Player "O"
                 activeBoard[boxPos] = playerOne;
                 $clickedBox.addClass("box-filled-1");
-                p1.className = "players";
-                p2.className = "players active";
+                winner = checkForWinner(activeBoard, playerOne);
+                $p1.removeClass("active");
+                $p2.addClass("active");
                 player += 1;
-                var winner = checkForWinner(activeBoard, playerOne);
             } else if (player === 2) {                  // Player "X"
                 activeBoard[boxPos] = playerTwo;
                 $clickedBox.addClass("box-filled-2");
-                p1.className = "players active";
-                p2.className = "players";
+                winner = checkForWinner(activeBoard, playerTwo);
+                $p1.addClass("active");
+                $p2.removeClass("active");
                 player -= 1;
-                var winner = checkForWinner(activeBoard, playerTwo);
             }
         }
         if (winner) {
@@ -102,21 +102,21 @@
     // --------------------------------------------------------------
     // CHECK FOR WINNER FUNCTION
     // --------------------------------------------------------------
-    function checkForWinner(board, player) {
-        var gamePattern = [];
-        board.forEach(function(el, index, arr) {
-            if (el === player) {
-                gamePattern.push(index);
+    function checkForWinner(board, currentPlayer) {
+        var playerPattern = [];
+        board.forEach(function(boardToken, index, arr) {
+            if (boardToken === currentPlayer) {
+                playerPattern.push(index);
             } 
         })
-        console.log(gamePattern);
+        console.log(playerPattern);
         // debugger
         var winner = null;
-        winPattern.forEach(function(value, index, array) {
+        winPattern.forEach(function(thisPattern, index, array) {
             // debugger
-            if(value.every(element => gamePattern.indexOf(element) > -1)) {
-                console.log("You won player " + player);
-                winner = {player: player};
+            if(thisPattern.every(element => playerPattern.indexOf(element) > -1)) {
+                console.log("You won player " + currentPlayer);
+                winner = {player: currentPlayer};
             }
         })
         return winner;
@@ -126,6 +126,7 @@
     // END OF GAME FUNCTION
     // --------------------------------------------------------------
     function endOfGame(winner) {
+        $(screenWin).removeClass("screen-win-one screen-win-two");
         $(screenWin).addClass("screen-win-" + winner.player);   //"screen-win-one", "screen-win-two", "screen-win-tie"
         var message = document.querySelector(".message");
         message.textContent = "Winner";
