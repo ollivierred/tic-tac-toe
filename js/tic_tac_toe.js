8ec5071482ad81b0be2f19ae211134b825013e67
ollivierred <ollivierre.dev@gmail.com>
2017-11-11 01:31:09 -0600

    commit @ 1:30AM Sat, Nov 11

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,16 +1,17 @@
-(function() {
-    var myGame = {
-        boardArr: [ "E", "E", "E", "E", "E", "E", "E", "E", "E" ],
-        winPattern: [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-            [0, 4, 8], [6, 4, 2] ],
-        turnCount: 1,
-        // Player "O"
-        playerOne: "one",
-        // Player "X"
-        playerTwo: "two",
+var ticTacToe = (function() {
 
+    var playersToken = "one";       // Player "O" = "one" || Player "X" = "two"
+    var turnCount = 1;
+    var gameState = {
+        gameOver: false, gameWinner: "" };
+    var boardArr = [ 
+        "E", "E", "E", "E", 
+        "E", "E", "E", "E", "E" ];
+    const winPattern = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+        [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2] ];
+
+    var myGame = {
         init: function() {
             this.domElements();
             this.bindEvents();
@@ -19,169 +20,128 @@
         domElements: function() {
             this.startScreen = document.querySelector("#start");
             this.winScreen = document.querySelector("#finish");
-            this.gameBoard = document.querySelector("#board");
-            this.boardSquares = document.querySelectorAll(".box");
+            this.boardContainer = document.querySelector("#board");
+            this.board = document.querySelector("#board .boxes");
+            this.boardSquares = document.querySelectorAll("#board .box");
             this.newGameBtn = document.querySelector("#finish a");
             this.startGameBtn = document.querySelector("#start a");
             this.$indicatorForP1 = $("#player1");
             this.$indicatorForP2 = $("#player2");
         },
         bindEvents: function() {
-            this.createEvent("click", this.startGameBtn, this.loadGame);
-            this.createEvent("click", this.newGameBtn, this.loadGame);
-            // this.createBoardEvents(boardSquares);
-
+            this.startGameBtn.addEventListener("click", this.loadBoard.bind(myGame), false);
+            this.newGameBtn.addEventListener("click", this.loadBoard.bind(myGame), false);
+            this.board.addEventListener("click", this.playersMove.bind(myGame), false);
+            this.board.addEventListener("mouseover", this.hoverStateOn.bind(myGame), false);
+            this.board.addEventListener("mouseout", this.hoverStateOff.bind(myGame), false);
         },
         render: function() {
-            console.log(myGame.boardSquares);
-            console.log(myGame.boardArr);
-
-
             this.showElement(this.startScreen);
             this.hideElement(this.winScreen);
-            this.hideElement(this.gameBoard);
-
-            myGame.boardSquares.forEach(function(square) {
-                console.log(square);
-                myGame.createEvent("click", square, myGame.playersMove);
-                myGame.createEvent("mouseover", square, myGame.showHoverToken);
-                myGame.createEvent("mouseout", square, myGame.hideHoverToken);
-                $(square).removeClass("box-filled-1 box-filled-2");
-                $(square).css("background-image", "");
-            })
-        },
-        loadGame: function() {
-
-            myGame.hideElement(myGame.startScreen);
-            myGame.showElement(myGame.gameBoard);
-
-
-        },
-        createEvent: function(eventType, element, func) {
-            element.addEventListener(eventType, func, false);
-        },
-        removeEvent: function(eventType, element, func) {
-            element.removeEventListener(eventType, func, false);
+            this.hideElement(this.boardContainer);
         },
         showElement: function(element) {
             element.style.display = "inherit";
-        },
-        showHoverToken: function() {
-    //     const hoverTarget = box.target;
-    //     var symbol;
-    //     // console.log(player);
-    //     // console.log(hoverTarget);
-    //     if (player === 1) symbol = "o";
-    //     if (player === 2) symbol = "x";
-    //     hoverTarget.style.backgroundImage = "url(img/" + symbol + ".svg)";
-        },
-        hideHoverToken: function() {
-    //     const hoverTarget = box.target;
-    //     hoverTarget.style.backgroundImage = "";            
         },
         hideElement: function(element) {
             element.style.display = "none";
         },
 
-        playersMove: function(element, currentPlayer) {
-            recordMove(element, currentPlayer);
-            placeToken(myGame.boardArr, currentPlayer);
+        loadBoard: function() {
+            this.boardSquares.forEach(function(square) {
+                // console.log(square);
+                $(square).removeClass("box-filled-1 box-filled-2");
+                $(square).css("background-image", "");
+            })
+
+            this.setActiveState();
+            this.hideElement(this.startScreen);
+            this.hideElement(this.winScreen);
+            this.showElement(this.boardContainer);
         },
-        recordMove: function(element, currentPlayer) {
-            const selectedSquare = element.target;
-            const selectedSquareIndex = element.target.getAttribute("data-pos");
-            console.log(selectedSquare);
-            console.log(selectedSquareIndex);
-            // Player "O" || Player "X"
-            myGame.boardArr[selectedSquareIndex] = currentPlayer;
-            console.log(myGame.boardArr);
+
+        playersMove: function(element) {
+            var square = element.target;
+            if ($(square).hasClass("box-filled-1") ||
+                $(square).hasClass("box-filled-2")) {
+                return;
+            } else {
+                this.placeToken(element.target, this.boardSquares, playersToken, boardArr);
+                this.toggleActiveState();
+                this.setActiveState();
+            }
         },
-        placeToken: function(arr, currentPlayer) {
+        placeToken: function(element, elementList, player, arr) {
+            const selectedSquare = element;
+            const selectedSquareIndex = element.getAttribute("data-pos");
+            // Adds move to array
+            arr[selectedSquareIndex] = player;
+            // Places token onto board
             arr.forEach(function(arrValue, index) {
-                if (arrValue === currentPlayer) {
-                    $(elementList[index]).addClass("box-filled-1");
+                if (arrValue === player) {
+                    if (player === "one") $(elementList[index]).addClass("box-filled-1");
+                    if (player === "two") $(elementList[index]).addClass("box-filled-2");
                 }
             })
         },
+        setActiveState: function() {
+            if (turnCount === 1) {
+                this.$indicatorForP1.addClass("players-turn active");
+                this.$indicatorForP2.removeClass("players-turn active");
+                playersToken = "one";
+            }
+            if (turnCount === 2) {
+                this.$indicatorForP2.addClass("players-turn active");
+                this.$indicatorForP1.removeClass("players-turn active");
+                playersToken = "two";
+            }
+            
+        },
+        toggleActiveState: function() {
+            if (turnCount === 1) {
+                turnCount += 1;
+                playersToken = "two";
+            } else if (turnCount === 2) {
+                turnCount -= 1;
+                playersToken = "one";
+            }
+        },
+
+        hoverStateOn: function(element) {
+            var square = element.target;
+            if ($(square).hasClass("box-filled-1") ||
+                $(square).hasClass("box-filled-2")) {
+                return;
+            } else {
+                this.enableHover(square, playersToken);
+            }
+        },
+        hoverStateOff: function(element) {
+            var square = element.target;
+            this.disableHover(square);
+        },
+        enableHover: function(element, player) {
+            var symbol = "o";
+            if (player === "one") symbol = "o";
+            if (player === "two") symbol = "x";
+            element.style.backgroundImage = "url(img/" + symbol + ".svg)";
+        },
+        disableHover: function(element) {
+            element.style.backgroundImage = "";            
+        },
+
     };
 
-    myGame.init();
+    return myGame;
 
 })();
 
-
-            
-
-            // this.removeEvent("click", selectedSquare, this.placePlayersToken);
-            // this.removeEvent("mouseover", selectedSquare, this.showHoverToken);
-            // this.removeEvent("mouseout", selectedSquare, this.hideHoverToken);
-    
-    // var state = {};
-    // var AI = {};
-    // var AIState = {};
-    // var game = {};
-    // var config = {
-    //         // --------------------------------------------------------------
-    // // GLOBAL VARIABLES
-    // // --------------------------------------------------------------
-    // // Screen elements
-    // const 
-    // const 
+    ticTacToe.init();
 
 
 
-
-    // // var originalBoard = [ "", "", "", "", "", "", "", "", "" ];
+    // var originalBoard = [ "", "", "", "", "", "", "", "", "" ];
     // originalBoard = [ "one", 1, "two", "two", 4, "two", 6, "one", "one" ];
-    // var player;
-    // var winner;
-    
-    // };
-    
-    // // --------------------------------------------------------------
-    // // REMOVE LISTENER FROM FILLED BOXES
-    // // --------------------------------------------------------------
-    // function hideToken(box) { 
-    //     const hoverTarget = box.target;
-    //     hoverTarget.style.backgroundImage = ""; 
-    // }
-    // // --------------------------------------------------------------
-    // // ADD HOVER TARGET FUNCTION
-    // // --------------------------------------------------------------
-    // function showToken(box)  {
-    //     const hoverTarget = box.target;
-    //     var symbol;
-    //     // console.log(player);
-    //     // console.log(hoverTarget);
-    //     if (player === 1) symbol = "o";
-    //     if (player === 2) symbol = "x";
-    //     hoverTarget.style.backgroundImage = "url(img/" + symbol + ".svg)";
-    // }
-
-    // // --------------------------------------------------------------
-    // // HELPER FUNCTIONS
-    // // --------------------------------------------------------------
-    // var helper = {
-    //     show: function(element) {
-    //         element.style.display = "inherit";
-    //     },
-
-    //     hide: function(element) {
-    //         element.style.display = "none";
-    //     },
-
-    //     // Creates event listeners
-    //     createEvent: function(element, eventType, func) {
-    //         element.addEventListener(eventType, func, false);
-    //     },
-
-    //     // Creates event listeners
-    //     removeListener: function(element, eventType, func) {
-    //         element.removeEventListener(eventType, func, false);
-    //     },
-
-
-    // } 
 
     // // --------------------------------------------------------------
     // // COMPUTER PLAYER "AI" RELATED FUNCTIONS
@@ -234,98 +194,11 @@
         
     // }
 
-    // function startGame() {
-    //     // originalBoard = [ "", "", "", "", "", "", "", "", "" ];
-    //     originalBoard = [ "one", 1, "two", "two", 4, "two", 6, "one", "one" ];
-    //     player = 1;       // Declares who goes first
-    //     winner = null;
-        
-    //     $p1.addClass("players-turn active");
-    //     $p2.removeClass("players-turn active");
-        
-    //     // boxEventListeners(boxes);
-    //     boxes.forEach(function(box, index, arr) {
-    //         $(box).removeClass("box-filled-1 box-filled-2");
-    //         $(box).css("background-image", "");
-    //         helper.createListener(box, "click", nextTurn);
-    //         helper.createListener(box, "mouseover", showToken);
-    //         helper.createListener(box, "mouseout", hideToken);
-    //     })
-
-    //     originalBoard.forEach(function(value) {
-    //         if (value === "one") {
-    //             helper.fillBox(originalBoard, "one", 1);
-    //             player +=1;
-    //         }
-    //         if (value === "two") {
-    //             helper.fillBox(originalBoard, "two", 2);
-    //             player -=1;
-    //         }
-    //     })
-
-    //     helper.hide(startScreen);
-    //     helper.hide(winScreen);
-    //     helper.show(gameBoard);
-    // }
-
-
-
-
-    // // Visibility of each screen
-    // helper.hide(gameBoard);
-    // helper.show(startScreen);
-    // helper.hide(winScreen);
-
-
-    // // createListener(window, "load", show(startScreen));
-    // helper.createListener(startGameButton, "click", startGame);
-
-
-
     // function computerWillMove() {
     //     // var boxIndex = computer.findEmptySpots(originalBoard)[0]; 
     //     var boxIndex = minMax(originalBoard, playerTwo).index;
     //     return boxes[boxIndex];
     // }
-
-    // // --------------------------------------------------------------
-    // // PLAYER'S TURN FUNCTION
-    // // --------------------------------------------------------------
-    // function nextTurn(box) {             // 
-    //     turn(box.target, playerOne);
-    //     if (winner === null) {
-    //         turn(computerWillMove(), playerTwo);                 // Player "X" / computer
-    //     }
-    // }
-    //     function turn(box, currentPlayer) {
-    //         const selectedBox = box;
-    //         const selectedBoxIndex = box.getAttribute("data-pos");
-    //         var winner = checkForWinner(originalBoard, currentPlayer);
-            
-    //         // Player "O" || Player "X"
-    //         originalBoard[selectedBoxIndex] = currentPlayer;
-
-    //         helper.removeListener(selectedBox, "click", nextTurn);
-    //         helper.removeListener(selectedBox, "mouseover", showToken);
-    //         helper.removeListener(selectedBox, "mouseout", hideToken);
-
-    //         if (currentPlayer === "one") {
-    //             $p1.removeClass("players-turn active");
-    //             $p2.addClass("players-turn active");
-    //             helper.fillBox(originalBoard, "one", 1);
-    //             player +=1;
-    //         }
-    //         if (currentPlayer === "two") {
-    //             $p2.removeClass("players-turn active");
-    //             $p1.addClass("players-turn active");
-    //             helper.fillBox(originalBoard, "two", 2);
-    //             player -=1;
-    //         }
-
-    //         if(winner.gameOver) endOfGame(winner);
-    //     }
-
-
 
 
     // // --------------------------------------------------------------
